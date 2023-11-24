774b9072877fd82aa32d1c41f683d10614032812
Guido <guido.maimone7@gmail.com>
2023-11-23 21:13:20 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("public"));
+
+const port = process.env.PORT || 3001
+app.listen(port, () => 
+    console.log(`Servidor corriendo en el puerto http://localhost:${port}`)
+);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+})
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+})
